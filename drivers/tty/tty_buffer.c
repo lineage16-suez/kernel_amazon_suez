18284fd284504f316bcf2f1c789ccb1aae0c38f0
ggow <9898181+ggow@users.noreply.github.com>
2022-04-13 09:21:16 +0100

    Revert "Resolve merge conflict for commit c428d528f8a0"
    This reverts commit cbab1977e64c83442f8b4d8e50844f2ace578656.

--- drivers/tty/tty_buffer.c
+++ drivers/tty/tty_buffer.c
@@ -414,8 +414,7 @@ receive_buf(struct tty_struct *tty, struct tty_buffer *head, int count)
 		if (count)
 			disc->ops->receive_buf(tty, p, f, count);
 	}
-	if (count > 0)
-		memset(p, 0, count);
+	head->read += count;
 	return count;
 }
 
